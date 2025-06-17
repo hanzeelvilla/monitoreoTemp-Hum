b44bafd8660be6eac853baf60df4ee3428d81183
Hanzeel Villa <hanzeelvilla0@gmail.com>
2025-06-17 10:15:56 -0600

    Función read_DHT11_data

--- monitoreoTemp-Hum.ino
+++ monitoreoTemp-Hum.ino
@@ -3,6 +3,15 @@
 #define DHTPIN 4
 #define DHTTYPE DHT11
 
+struct DHT11_data {
+  float temperatura;
+  float humedad;
+};
+
+
+DHT11_data read_DHT11_data();
+
+DHT11_data data;
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
@@ -11,13 +20,19 @@ void setup() {
 }
 
 void loop() {
-  float humedad = dht.readHumidity();
-  float temp = dht.readTemperature();
+  data = read_DHT11_data();
 
   Serial.print("Humedad: ");
-  Serial.println(humedad);
+  Serial.println(data.humedad);
   Serial.print("Temperatura: ");
-  Serial.println(temp);
+  Serial.println(data.temperatura);
 
-  delay(1000);
+  delay(1000); 
+}
+
+DHT11_data read_DHT11_data() {
+  DHT11_data data;
+  data.temperatura = dht.readTemperature();
+  data.humedad = dht.readHumidity();
+  return data;
 }
